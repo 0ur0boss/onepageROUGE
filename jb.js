2e62c6fdab2e5e9045adbceebf7b8f56710da9a5
0ur0boss <enzoduret@yahoo.fr>
2021-10-08 09:51:19 +0200

    hrefnavbarmarche!

--- jb.js
+++ jb.js
@@ -5,22 +5,6 @@ ScrollIndicator = (function(){
 	var scrollIndicator,
 			sections;
 	
-	function onSectionClick(e) {
-		
-		e.preventDefault();
-		
-		if (!/li|a/.test(e.target.nodeName.toLowerCase())) {
-			return false;
-		}
-		
-		ScrollIt(e.target);
-		
-	}
-	
-	function addEventListeners() {
-		scrollIndicator.addEventListener('click', onSectionClick);
-	}
-	
 	function update(val) {
 		
 		var winOffset = window.innerHeight * 0.25;
@@ -82,7 +66,6 @@ ScrollIndicator = (function(){
 		
 		scrollIndicator.appendChild(ul);
 		
-		addEventListeners();
 	}
 	
 	document.addEventListener('DOMContentLoaded', init);
@@ -110,39 +93,3 @@ GlobalScroll = (function() {
 	document.addEventListener('DOMContentLoaded', scrollVal);
 	
 })();
-
-
-
-ScrollIt = function(target, time) {
-	
-	var targetId = target.href.split('#')[1],
-			end = document.getElementById(targetId).offsetTop,
-			start = window.pageYOffset,
-			duration = time || 1000,
-			distance = end - start,
-			timeStart = Date.now(),
-			timeEnd = timeStart + duration,
-			tick, cachedTick;
-	
-	//https://en.wikipedia.org/wiki/Smoothstep
-	function step(startTime, endTime, point) {
-		if (point <= startTime) { return 0; }
-		if (point >= endTime) { return 1; }
-		var x = (point - startTime) / (endTime - startTime);
-		return x*x*x*(x*(x*6 - 15) + 10);
-	}
-	
-  (function loop() {
-	
-		var now = Date.now(),
-				tick = start + (distance * step(timeStart, timeEnd, now));
-		
-		window.scrollTo(0, Math.floor(tick));
-
-		if (now <= timeEnd) {
-			requestAnimationFrame(loop);
-		}
-		
-  })();
-  
-}
